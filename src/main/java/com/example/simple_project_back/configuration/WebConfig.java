7b87ef5b32b8f5734d9a29d6edb79d8df6e48875
Yithian01 <dbwlsdud10@naver.com>
2024-08-01 19:41:14 +0900

    2024/08/01_beforechenge_main

--- src/main/java/com/example/simple_project_back/configuration/WebConfig.java
+++ src/main/java/com/example/simple_project_back/configuration/WebConfig.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/**")
+                .allowedOrigins("http://localhost:3000") // 프론트엔드 도메인 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowCredentials(true)
                 .maxAge(3600);
