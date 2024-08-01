e62c471bfc58b3bbec790b4da2adc62c5465a4fd
Yithian01 <dbwlsdud10@naver.com>
2024-08-01 17:16:31 +0900

    2024/08/01_jin_merge_config_stting

--- src/main/java/com/example/simple_project_back/configuration/WebConfig.java
+++ src/main/java/com/example/simple_project_back/configuration/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "FETCH")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
